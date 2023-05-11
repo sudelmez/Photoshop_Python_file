405584e660fa3db45745405824042062b281bc2e
Sude Ölmez <98457237+sudelmez@users.noreply.github.com>
2023-05-11 20:44:14 +0300

    Update photo_edit.py

--- photo_edit.py
+++ photo_edit.py
@@ -5,9 +5,5 @@ def photoGet(name):
     image = cv2.imread("uploaded_images/"+name,0) 
     cv2.imwrite("uploaded_images/"+name,image) 
     
-    cv2.imshow("new image",image)
-    
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    cv2.waitKey(1)
+ 
 
