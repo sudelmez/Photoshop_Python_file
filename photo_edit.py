a3b140e880e1e02fe1e11b857fb5826faca8c91e
Sude Ölmez <98457237+sudelmez@users.noreply.github.com>
2023-05-12 15:48:25 +0300

    Update photo_edit.py

--- photo_edit.py
+++ photo_edit.py
@@ -1,9 +1,9 @@
 import cv2
 import numpy as np 
 
-def photoGet(name):
-    image = cv2.imread("uploaded_images/"+name,0) 
-    cv2.imwrite("uploaded_images/"+name,image) 
-    
- 
+def getShop(image):
+    img = image
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY )
+    return img
+
 
